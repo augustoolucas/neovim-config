d479c8cec623eb3d0a00aea3e924dd059017376a
Lucas Alcantara <lama2@cin.ufpe.br>
2023-08-04 23:58:48 -0300

    formatting the interface before startup, using nvim-tree instead of Vexplore, fixing nvim exit with opened terminal buffer

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -18,9 +18,24 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 -- Automatically close tab/vim when nvim-tree is the last window in the tab
 vim.cmd "autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif"
 
+--[[
 vim.api.nvim_create_autocmd({ "VimResized" }, {
   callback = function()
     vim.cmd "tabdo wincmd ="
+  end,
+})
+--]]
+
+vim.api.nvim_create_autocmd({ "VimLeavePre" }, {
+    callback = function()
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      if vim.api.nvim_buf_get_option(buf, 'buftype') == 'terminal' then
+        vim.api.nvim_buf_call(buf, function()
+          vim.cmd [[stopinsert]]
+          vim.fn.chansend(vim.b.terminal_job_id, "exit\n")
+        end)
+      end
+    end
   end,
 })
 
@@ -37,10 +52,31 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   end,
 })
 
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+  callback = function()
+  if vim.g.my_callback_has_run then
+      return
+  end
+
+  local height = vim.api.nvim_win_get_height(0)
+  local res = math.max(1, math.floor(height * 0.15))
+
+  vim.cmd [[NvimTreeToggle]]
+  vim.cmd [[wincmd l]]
+  vim.cmd [[split]]
+  vim.cmd [[term zsh]]
+  vim.api.nvim_command("res " .. tostring(res))
+
+  vim.g.my_callback_has_run = true
+  vim.cmd [[wincmd p]]
+  end,
+})
+
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
   callback = function()
     vim.cmd "hi link illuminatedWord LspReferenceText"
-  end,
+    vim.cmd [[wincmd l]]
+  end
 })
 
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
@@ -51,3 +87,6 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
     end
   end,
 })
+
+vim.api.nvim_command("command! Z w | qa")
+vim.api.nvim_command("cabbrev wqa Z")
--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -2,6 +2,7 @@ local M = {
   "kyazdani42/nvim-tree.lua",
   commit = "f5d970d4506f385b29534252d8c15a782fa53034",
   event = "VimEnter",
+  lazy = false
 }
 
 local function on_attach(bufnr)
