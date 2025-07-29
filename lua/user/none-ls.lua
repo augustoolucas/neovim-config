cf9657d7a7fbb1374346595ce18eae2654dff706
Lucas Alcantara <lama2@cesar.org.br>
2025-07-28 21:25:06 -0300

    refactor(lint): Migrate none-ls setup to use mason-null-ls and ruff

--- lua/user/none-ls.lua
+++ lua/user/none-ls.lua
@@ -2,40 +2,37 @@ local M = {
   "nvimtools/none-ls.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvimtools/none-ls-extras.nvim"
+    "nvimtools/none-ls-extras.nvim",
+    "jayp0521/mason-null-ls.nvim",
   },
 }
 
 function M.config()
-  local null_ls = require "null-ls"
+  require('mason-null-ls').setup {
+    ensure_installed = {
+      'ruff',
+      'prettier',
+      'shfmt',
+    }
+  }
 
-  local formatting = null_ls.builtins.formatting
-  local diagnostics = null_ls.builtins.diagnostics
+  local null_ls = require "null-ls"
+  local sources = {
+    require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I' } },
+    require 'none-ls.formatting.ruff_format',
+    null_ls.builtins.formatting.prettier.with { filetypes = { 'json', 'yaml', 'markdown' } },
+    null_ls.builtins.formatting.shfmt.with { args = { '-i', '4' } },
+  }
+
   local on_attach = function(client, bufnr)
       local opts = { buffer = bufnr, noremap = true, silent = true }
-      -- Atalho para ver documentação flutuante (Hover)
-      -- vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-      -- Atalho para Ações de Código (Code Actions), crucial para o none-ls
-      --vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-      -- Adicione outros atalhos de LSP que você usa (gd, gr, etc.)
     end
 
   null_ls.setup {
     debug = false,
-    sources = {
-      formatting.stylua,
-      formatting.prettier,
-      formatting.isort,
-      formatting.black,
-      -- formatting.prettier.with {
-      --   extra_filetypes = { "toml" },
-      --   -- extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-      -- },
-      -- formatting.eslint,
-      diagnostics.pylint,
-      null_ls.builtins.completion.spell,
-    },
+    sources = sources,
     on_attach = on_attach,
+
   }
 end
 
