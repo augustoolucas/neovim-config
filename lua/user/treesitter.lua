ba45c0b96fb04b3efabef0c0e0eb33aad0c0ac4d
Lucas Alcantara <lama2@cesar.org.br>
2025-09-22 17:30:51 -0300

    adding yaml parser

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -6,7 +6,7 @@ local M = {
 
 function M.config()
   require("nvim-treesitter.configs").setup {
-    ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python" },
+    ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python", "yaml" },
     highlight = { enable = true },
     indent = { enable = true },
   }
