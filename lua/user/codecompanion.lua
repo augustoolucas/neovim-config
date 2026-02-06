635e2273c54a27386c537433e81f01a933462bdc
Lucas Alcantara <lama2@cesar.org.br>
2026-02-06 14:16:48 -0300

    removing multiple providers

--- lua/user/codecompanion.lua
+++ lua/user/codecompanion.lua
@@ -6,22 +6,6 @@ local M = {
   },
 }
 
-local function get_gemini_api_key()
-  local handle = io.popen("pass show gemini_api_key")
-  if not handle then
-    print("Could not execute 'pass show gemini_api_key'")
-    return nil
-  end
-  local api_key = handle:read("*a")
-  handle:close()
-  if api_key then
-    return string.sub(api_key, 1, string.len(api_key) - 1)
-  else
-    print("Could not read API key from app pass")
-    return nil
-  end
-end
-
 function M.config()
   require("codecompanion").setup({
     display = {
@@ -31,13 +15,13 @@ function M.config()
     },
     strategies = {
       chat = {
-        adapter = "gemini"
+        adapter = "openai"
       },
       inline = {
-        adapter = "gemini"
+        adapter = "openai"
       },
       cmd = {
-        adapter = "gemini"
+        adapter = "openai"
       },
     },
     adapters = {
@@ -45,48 +29,11 @@ function M.config()
         opts = {
           show_model_choices = true,
         },
-        qwen2 = function()
-          return require("codecompanion.adapters").extend("ollama", {
-            opts = {
-              stream = true,
-            },
-            schema = {
-              model = {
-                default = "hf.co/unsloth/Qwen2.5-Coder-3B-Instruct-GGUF:Q4_K_M"
-              },
-            },
-          })
-        end,
-        gemini = function()
-          return require("codecompanion.adapters").extend("gemini", {
-            schema = {
-              model = {
-                default = "gemini-2.5-pro",
-              },
-            },
-            env = {
-              api_key = get_gemini_api_key(),
-            },
-          })
-        end,
-        azure_openai = function()
-          return require("codecompanion.adapters").extend("azure_openai", {
-            schema = {
-              model = {
-                default = "YOUR_DEPLOYMENT_NAME",
-              },
-            },
-            env = {
-              api_key = "API_KEY",
-              endpoint = "YOUR_AZURE_OPENAI_ENDPOINT",
-            },
-          })
-        end,
         openai = function()
           return require("codecompanion.adapters").extend("openai", {
             schema = {
               model = {
-                default = "gpt-4.1",
+                default = "gpt-5.1",
               },
             },
             env = {
