005c9eb46707dac038397904504dec455c20ea2a
Lucas Alcantara <lama2@cesar.org.br>
2026-03-18 18:08:51 -0300

    chore(telescope): disable colorscheme/projects/help keymaps

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -14,11 +14,11 @@ function M.config()
   wk.add {
     { "<leader>bb", "<cmd>Telescope buffers previewer=false<cr>", desc = "Find" },
     { "<leader>fb", "<cmd>Telescope git_branches<cr>", desc = "Checkout branch" },
-    { "<leader>fc", "<cmd>Telescope colorscheme<cr>", desc = "Colorscheme" },
+    -- { "<leader>fc", "<cmd>Telescope colorscheme<cr>", desc = "Colorscheme" },
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-    { "<leader>fp", "<cmd>lua require('telescope').extensions.projects.projects()<cr>", desc = "Projects" },
+    -- { "<leader>fp", "<cmd>lua require('telescope').extensions.projects.projects()<cr>", desc = "Projects" },
     { "<leader>ft", "<cmd>Telescope live_grep<cr>", desc = "Find Text" },
-    { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help" },
+    -- { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help" },
     { "<leader>fl", "<cmd>Telescope resume<cr>", desc = "Last Search" },
     { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent File" },
   }
