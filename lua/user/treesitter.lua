cf408f7e00ca45a5c55192dd5f0024374e276084
Lucas Alcantara <lucasaugusto096@gmail.com>
2026-04-25 23:26:22 -0300

    fix: set treesitter branch to main to work on nvim 0.12

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,11 +1,12 @@
 local M = {
   "nvim-treesitter/nvim-treesitter",
   event = { "BufReadPost", "BufNewFile" },
+  branch = "main",
   build = ":TSUpdate",
 }
 
 function M.config()
-  require("nvim-treesitter.configs").setup {
+  require("nvim-treesitter.config").setup {
     ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python", "yaml" },
     highlight = { enable = true },
     indent = { enable = true },
