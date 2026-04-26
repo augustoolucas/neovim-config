9fac8c81997e818b56e51d6e358075a028bcdc55
Lucas Alcantara <lucasaugusto096@gmail.com>
2026-04-25 23:46:44 -0300

    removing gemini comments

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -1,14 +1,12 @@
 local M = {
   "lukas-reineke/indent-blankline.nvim",
   event = { "BufReadPost", "BufNewFile" },
-  -- Removemos a linha "commit = ..." para o Lazy baixar a versão mais atual (v3)
   main = "ibl",
 }
 
 function M.config()
   local icons = require "user.icons"
 
-  -- Na versão 3, o nome do módulo mudou para 'ibl' e as opções foram simplificadas
   require("ibl").setup {
     indent = { 
       char = icons.ui.LineMiddle,
